503e41d915c55c7defe4a2229c3ef0d80c593217
David Ballesteros <dballesg@gmail.com>
2017-03-19 12:33:36 +0100

    First commit of conanfiles before start editing.

---
+++ test_package/example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+
+int main() {
+    hello();
+    std::cout<<"*** Running example, will fail by default, implement yours! ***\n";
+    return -1; // fail by default, remember to implement your test
+}
